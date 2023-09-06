02b731fbe4db63be1db5e245aa9f484ba1096d19
CarolLu85 <little_shrewdness@hotmail.com>
2023-09-06 20:18:07 +0300

    add a while loop and fixed a big bug

--- Exercise3.java
+++ Exercise3.java
@@ -4,17 +4,26 @@ public class Exercise3 {
     public static void main(String[] args) {
         Random random = new Random();
         int[] number = new int[5];
-        for (int i = 0; i < 5; i++) {
-            number[i] = random.nextInt(20) + 1;
-            for (int a = 0; a < 5; a++) {
-                if (number[i] == number[a]) {
+        boolean same_number = false;
+        int picked_number = 0;
+        int index = 0;
+        while (true) {
+            picked_number = random.nextInt(20) + 1;
+            for (int i = 0; i <= index; i++) {
+                if (picked_number == number[i]) {
+                    same_number = true;
                     break;
+                } else {
+                    number[index] = picked_number;
                 }
+            }
+            index = index + 1;
+            if (index == 5) {
+                break;
             }
         }
         for (int b = 0; b < 5; b++) {
-            System.out.println(number[b]);
+        System.out.println(number[b]);
         }
-
     }
 }
