62e1ff36a833febd637e8c1326414eeabc166065
CarolLu85 <little_shrewdness@hotmail.com>
2023-09-06 18:14:58 +0300

    fixed a bug in the first "for" loop

--- Exercise3.java
+++ Exercise3.java
@@ -4,17 +4,17 @@ public class Exercise3 {
     public static void main(String[] args) {
         Random random = new Random();
         int[] number = new int[5];
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             number[i] = random.nextInt(20) + 1;
-            for (int a = 0; a < 7; a++){
-                if(number[i] == number[a]){
+            for (int a = 0; a < 5; a++) {
+                if (number[i] == number[a]) {
                     break;
                 }
             }
         }
-    for (int b = 0; b < 5; b++){
-        System.out.println(number[b]);
-    }    
+        for (int b = 0; b < 5; b++) {
+            System.out.println(number[b]);
+        }
 
-    }   
+    }
 }
