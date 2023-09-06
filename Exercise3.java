dc3c738b99edcdb38115a2beef2d37ea50377321
CarolLu85 <little_shrewdness@hotmail.com>
2023-09-06 15:53:57 +0300

    a new java class created and finished coding

---
+++ Exercise3.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] number = new int[5];
+        for (int i = 0; i < 5; i++){
+            number[i] = random.nextInt(20) + 1;
+            for (int a = 0; a < 7; a++){
+                if(number[i] == number[a]){
+                    break;
+                }
+            }
+        }
+
+
+    }   
+}
